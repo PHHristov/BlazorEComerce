5595f9dfc7a5aed9e14189e358be8282a18b9897
= <=>
2022-06-07 09:01:57 +0300

    product details for an item

---
+++ BlazorEComerce/Client/Pages/ProductDetails.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorEComerce.Client.Pages
+{
+    public partial class ProductDetails
+    {
+        private Product? product = null;
+
+        [Parameter]
+        public int Id { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            product = productService.Products.Find( p => p.Id == Id );
+        }
+    }
+}
+
