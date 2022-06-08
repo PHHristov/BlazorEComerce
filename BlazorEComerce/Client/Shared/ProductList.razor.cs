e626ac66f5328159cbc42740b0cbd3c00f840905
= <=>
2022-06-08 09:59:37 +0300

    show price in product list

--- BlazorEComerce/Client/Shared/ProductList.razor.cs
+++ BlazorEComerce/Client/Shared/ProductList.razor.cs
@@ -14,6 +14,21 @@ namespace BlazorEComerce.Client.Shared
         {
             ProductService.ProductsChanged -= StateHasChanged;
         }
+        private string GetPriceTag(Product product)
+        {
+            var variants = product.Variants;
+            if (variants.Count == 0)
+            {
+                return string.Empty;
+            }
+            else if (variants.Count == 1)
+            {
+                return $"{variants[0].Price}";
+            }
+
+            decimal minPrice = variants.Min(v => v.Price);
+            return $"Starting at {minPrice}";
+        }
 
     }
 }
