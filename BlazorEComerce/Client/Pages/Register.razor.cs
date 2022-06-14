dabb0cd6a2cc3bd95c85892b6a9b97e8b24a0750
= <=>
2022-06-14 08:32:01 +0300

    register page + user register model

---
+++ BlazorEComerce/Client/Pages/Register.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorEComerce.Client.Pages
+{
+    public partial class Register
+    {
+        UserRegister user = new UserRegister();
+
+        void HandleRegistration()
+        {
+            Console.WriteLine($"Register the User with Email {user.Email}");
+        }
+    }
+}
