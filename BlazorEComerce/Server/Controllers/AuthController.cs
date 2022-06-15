abc8f212e1cde935b99b5ea3a663d73440464c24
= <=>
2022-06-15 21:37:09 +0300

    cahnge password controller server side

--- BlazorEComerce/Server/Controllers/AuthController.cs
+++ BlazorEComerce/Server/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BlazorEComerce.Server.Controllers
 {
@@ -39,6 +41,20 @@ namespace BlazorEComerce.Server.Controllers
                 return BadRequest(response);
             }
 
+            return Ok(response);
+        }
+
+        [HttpPost("cahnge-password"), Authorize]
+        public async Task<ActionResult<ServiceResponse<bool>>> ChangePassword([FromBody] string newPassword)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var response = await _authService.ChangePassword(int.Parse(userId), newPassword);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            
             return Ok(response);
         }
     }
