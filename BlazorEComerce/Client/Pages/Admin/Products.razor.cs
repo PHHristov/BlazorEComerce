3104a53ad05b905540c227d4a0ccbd028367c638
= <=>
2022-07-03 20:15:16 +0300

    Admin Product Page

---
+++ BlazorEComerce/Client/Pages/Admin/Products.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorEComerce.Client.Pages.Admin
+{
+    public partial class Products
+    {
+        protected override async Task OnInitializedAsync()
+        {
+            await ProductService.GetAdminProducts();
+        }
+
+        void EditProduct(int productId)
+        {
+
+        }
+    }
+}
