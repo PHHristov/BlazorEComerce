bc9e3aaa7e0a12e14c486666ca06d408bccf221d
= <=>
2022-07-03 15:46:33 +0300

    producttype services server

---
+++ BlazorEComerce/Client/Pages/Admin/ProductTypes.razor.cs
@@ -0,0 +1,29 @@
+﻿namespace BlazorEComerce.Client.Pages.Admin
+{
+    public partial class ProductTypes
+    {
+        ProductType editingProductType = null;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await ProductTypeService.GetProductTypes();
+        }
+
+        private void EditProductType(ProductType productType)
+        {
+            productType.Editing = true;
+            editingProductType = productType;
+
+        }
+
+        private void CreateNewProductType()
+        {
+
+        }
+
+        private void UpdateProuctType()
+        {
+
+        }
+    }
+}
--- BlazorEComerce/Server/Services/ProductTypeService/ProductTypeService.cs
+++ BlazorEComerce/Server/Services/ProductTypeService/ProductTypeService.cs
@@ -9,10 +9,32 @@
             _context = context;
         }
 
+        public async Task<ServiceResponse<List<ProductType>>> AddProductType(ProductType productType)
+        {
+            _context.ProductTypes.Add(productType);
+            await _context.SaveChangesAsync();
+
+            return await GetProductTypes();
+        }
+
         public async Task<ServiceResponse<List<ProductType>>> GetProductTypes()
         {
             var products = await _context.ProductTypes.ToListAsync();
             return new ServiceResponse<List<ProductType>> { Data = products };
+        }
+
+        public async Task<ServiceResponse<List<ProductType>>> UpdateProductType(ProductType productType)
+        {
+            var dbProductType = await _context.ProductTypes.FindAsync(productType.Id);
+            if (dbProductType == null)
+            {
+                return new ServiceResponse<List<ProductType>> { Success = false, Message = "ProductType not found!" };
+            }
+
+            dbProductType.Name = productType.Name;
+            await _context.SaveChangesAsync();
+
+            return await GetProductTypes();
         }
     }
 }
